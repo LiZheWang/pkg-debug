f681600dcdaab40ac58b1a5396cd8544145c8190
wanglizhe <neocrm>
2022-03-04 15:31:20 +0800

    feat: 更新文档

--- .umirc.ts
+++ .umirc.ts
@@ -5,14 +5,17 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'page',
+  outputPath: 'docs',
   mode: 'site',
+  resolve: {
+    includes: ['mds', 'src'],
+  },
   // more config: https://d.umijs.org/config
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
       title: 'GitHub',
-      path: 'https://github.com/umijs/dumi',
+      path: 'https://github.com/LiZheWang/pkg-debug',
     },
   ],
 });
